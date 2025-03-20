5031474bf0b306e56147f33febb696fc81976891
daiki <daikihi+github@gmail.com>
2025-03-20 23:39:31 +0900

    execution can be switched

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 mod ping_pong;
+mod lamport_clock;
+use std::env;
+
 
 fn main() {
-    ping_pong::simple_ping_pong::run();
-    println!("switch to multiple ping pong");
-    ping_pong::multiple_ping_pong::run();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {} <mode>", args[0]);
+        println!("Modes:");
+        println!("  simple   - Run simple ping pong");
+        println!("  multiple - Run multiple ping pong");
+        return;
+    }
+
+    match args[1].as_str() {
+        "simple" => ping_pong::simple_ping_pong::run(),
+        "multiple" => ping_pong::multiple_ping_pong::run(),
+        _ => {
+            println!("Invalid mode: {}", args[1]);
+            println!("Usage: {} <mode>", args[0]);
+            println!("Modes:");
+            println!("  simple   - Run simple ping pong");
+            println!("  multiple - Run multiple ping pong");
+        }
+    }
 }
